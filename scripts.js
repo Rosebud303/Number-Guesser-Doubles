07099c1f92b2bfaa789682cb59677745d4d61668
Duy Vu <duyv.thanh@gmail.com>
2018-10-10 14:25:25 -0600

    Add css and javascript

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var minBox        =   document.querySelector('.min-box');
+var maxbox        =   document.querySelector('.max-box');
+var updateButton  =   document.querySelector('.update-button');
+var guessBox      =   document.querySelector('.guess-box1')
+var submitButton  =   document.querySelector('.submit-button')
+var clearButton   =   document.querySelector('.clear-button')
+var resetButton   =   document.querySelector('.reset-button')
